dbf11733fe3d94c3619a4dc7666af4fe51d7ca1f
grooveIsAll <bobby@backer.com>
2023-05-16 13:11:55 -0700

    change back

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,8 +3,7 @@ const path = require("path");
 
 import type { StorybookConfig } from "@storybook/react-vite";
 const config: StorybookConfig = {
-  // stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  stories: ['../src/components/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
