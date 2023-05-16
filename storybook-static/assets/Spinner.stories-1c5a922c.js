459ce071124c7d52e372f798f01a51ed262689b1
grooveIsAll <bobby@backer.com>
2023-05-16 13:22:10 -0700

    config

---
+++ storybook-static/assets/Spinner.stories-1c5a922c.js
@@ -0,0 +1,18 @@
+import{j as r,c as i}from"./index-b477a049.js";import"./index-c013ead5.js";import"./_commonjsHelpers-725317a4.js";const N="_loaderWrap_1gbp5_1",w="_loader_1gbp5_1",j="_rotate_1gbp5_1",P="_path_1gbp5_12",k="_backPath_1gbp5_19",L="_small_1gbp5_26",V="_medium_1gbp5_31",W="_large_1gbp5_36",q="_xlarge_1gbp5_41",a={loaderWrap:N,loader:w,rotate:j,path:P,backPath:k,small:L,"dash-small":"_dash-small_1gbp5_1",medium:V,large:W,"dash-medium":"_dash-medium_1gbp5_1",xlarge:q,"dash-large":"_dash-large_1gbp5_1"},n=({className:s,style:v,size:m="medium"})=>{const e=M(m);return r.jsx("div",{className:i(a.loaderWrap,s),style:{...v,width:e,height:e},children:r.jsxs("svg",{width:e,height:e,viewBox:`0 0 ${e} ${e}`,fill:"none",className:i(a.loader,a[m]),xmlns:"http://www.w3.org/2000/svg",children:[r.jsx("circle",{className:a.path,cx:e/2,cy:e/2,r:e/2-3}),r.jsx("circle",{className:a.backPath,cx:e/2,cy:e/2,r:e/2-3})]})})};n.defaultProps={className:"",style:{},size:"large"};const M=s=>{switch(s){case"small":return 24;case"medium":return 32;case"large":return 40;case"xlarge":return 80;default:return 32}};try{n.displayName="Spinner",n.__docgenInfo={description:"",displayName:"Spinner",props:{size:{defaultValue:{value:"medium"},description:"",name:"size",required:!1,type:{name:"enum",value:[{value:'"small"'},{value:'"medium"'},{value:'"large"'},{value:'"xlarge"'}]}},className:{defaultValue:null,description:"",name:"className",required:!1,type:{name:"string"}},style:{defaultValue:null,description:"",name:"style",required:!1,type:{name:"CSSProperties"}}}}}catch{}const B={title:"Misc/Spinner",component:n,tags:["autodocs"]},t={args:{size:"small"}},l={args:{size:"medium"}},c={args:{size:"large"}},o={args:{size:"xlarge"}};var d,p,g;t.parameters={...t.parameters,docs:{...(d=t.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    size: 'small'
+  }
+}`,...(g=(p=t.parameters)==null?void 0:p.docs)==null?void 0:g.source}}};var u,_,h;l.parameters={...l.parameters,docs:{...(u=l.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    size: 'medium'
+  }
+}`,...(h=(_=l.parameters)==null?void 0:_.docs)==null?void 0:h.source}}};var x,b,y;c.parameters={...c.parameters,docs:{...(x=c.parameters)==null?void 0:x.docs,source:{originalSource:`{
+  args: {
+    size: 'large'
+  }
+}`,...(y=(b=c.parameters)==null?void 0:b.docs)==null?void 0:y.source}}};var S,f,z;o.parameters={...o.parameters,docs:{...(S=o.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  args: {
+    size: 'xlarge'
+  }
+}`,...(z=(f=o.parameters)==null?void 0:f.docs)==null?void 0:z.source}}};const C=["Small","Medium","Large","XLarge"];export{c as Large,l as Medium,t as Small,o as XLarge,C as __namedExportsOrder,B as default};
+//# sourceMappingURL=Spinner.stories-1c5a922c.js.map
