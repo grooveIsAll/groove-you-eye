d0e7f21dcfdea62ee0b2cb236cc7972501c7e647
grooveIsAll <bobby@backer.com>
2023-05-16 17:29:52 -0700

    add scss module storybook package

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,3 +1,6 @@
+const { mergeConfig } = require("vite");
+const path = require("path");
+
 import type { StorybookConfig } from "@storybook/react-vite";
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
@@ -5,7 +8,22 @@ const config: StorybookConfig = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+    "storybook-css-modules"
   ],
+  core: {
+    builder: '@storybook/builder-vite',
+  },
+  async viteFinal(config) {
+    return mergeConfig(config, {
+      resolve: {
+        rules: {
+          test: /\.scss$/,
+          use: ["style-loader", "css-loader", "sass-loader"],
+          include: path.resolve(__dirname, "../src"),
+        }
+      },
+    });
+  },
   framework: {
     name: "@storybook/react-vite",
     options: {},
