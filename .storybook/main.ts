fa4d32a730a75b9191ff075f65b31e2ad626526a
grooveIsAll <bobby@backer.com>
2023-05-16 17:09:33 -0700

    rever storybook main

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,29 +1,11 @@
-const { mergeConfig } = require("vite");
-const path = require("path");
-
 import type { StorybookConfig } from "@storybook/react-vite";
 const config: StorybookConfig = {
-  // stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  stories: ['../src/components/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
   ],
-  core: {
-    builder: '@storybook/builder-vite',
-  },
-  async viteFinal(config) {
-    return mergeConfig(config, {
-      resolve: {
-        rules: {
-          test: /\.scss$/,
-          use: ["style-loader", "css-loader", "sass-loader"],
-          include: path.resolve(__dirname, "../"),
-        }
-      },
-    });
-  },
   framework: {
     name: "@storybook/react-vite",
     options: {},
